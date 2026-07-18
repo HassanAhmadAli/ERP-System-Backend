842bd5913eb61c22f4f127a2697fd873049fa2fd
Hassan Ali <hassan.ali.36900@outlook.com>
2026-07-18 15:45:57 +0300

    improved health check

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -3,15 +3,18 @@ import { ApiTags } from "@nestjs/swagger";
 import { Public } from "@/common/decorators/public.decorator";
 import { DocumentOkResponse, DocumentOperation } from "@/openapi/decorators";
 import { HealthResponseDto } from "@/openapi/dto/responses.dto";
-
+import { PrismaService } from "@/prisma/prisma.service";
+import { Prisma } from "@/prisma/client";
 @ApiTags("Health")
 @Controller("health")
 export class HealthController {
+  constructor(private readonly prismaService: PrismaService) {}
   @Public()
   @Get()
   @DocumentOperation("Health check")
   @DocumentOkResponse("Service is running", HealthResponseDto)
-  check() {
+  async check() {
+    await this.prismaService.client.$executeRaw(Prisma.sql`SELECT 1`);
     return { status: "ok" };
   }
 }
